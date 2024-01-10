c191b6adc81dd10cf38fdca48546345d28298d96
cloudblaze123 <cloudblaze123@gmail.com>
2024-01-10 12:23:26 +0800

    将scroll_keyboard_container的代码从piano_keyboard中移出，将piano_keyboard和页面index_web_component修改回移入前的状态
    调整键盘控制栏界面，让控制栏和内部按钮的大小更加合理，添加放大，缩小图标，同时尝试新的按钮颜色。

--- src/component/piano-keyboard/web_component/piano-keyboard-template.ts
+++ src/component/piano-keyboard/web_component/piano-keyboard-template.ts
@@ -4,35 +4,11 @@ import { createOctaveTemplate } from "./createOctaveTemplate.js"
 
 export const pianoKeyboardTemplate=/*html*/
 `
-
-
-<div class="keyboards_container">
-            <div class="scroll_keyboard_component" enable_scroll>
-                <div class="scrollable_container">
-                    <div class="keyboard_container">
-                        <div class="piano_keyboard" hide_pitch_tag>
-                            ${createOctaveTemplate(2)}
-                            ${createOctaveTemplate(3)}
-                            ${createOctaveTemplate(4)}
-                            ${createOctaveTemplate(5)}
-                            ${createOctaveTemplate(6)}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="scroll_keyboard_component" enable_scroll>
-                <div class="scrollable_container">
-                    <div class="keyboard_container">
-                        <div class="piano_keyboard" hide_pitch_tag>
-                            ${createOctaveTemplate(2)}
-                            ${createOctaveTemplate(3)}
-                            ${createOctaveTemplate(4)}
-                            ${createOctaveTemplate(5)}
-                            ${createOctaveTemplate(6)}
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="piano_keyboard">
+    ${createOctaveTemplate(2)}
+    ${createOctaveTemplate(3)}
+    ${createOctaveTemplate(4)}
+    ${createOctaveTemplate(5)}
+    ${createOctaveTemplate(6)}
 </div>
-
 `
