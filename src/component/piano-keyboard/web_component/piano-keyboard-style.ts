9b91c98a9875a7fd4632f1b84855311925bb933c
cloudblaze123 <cloudblaze123@gmail.com>
2024-01-14 19:18:35 +0800

    主要变化： 1.引入parcel打包工具，项目开始使用parcel进行打包 2.同时调整.gitignore以适应新的项目结构
    次要变化：
    1.使用URL类表示MusicService中的音频路径，以便parcel识别和打包相应音频文件
    2.将piano_keyboard组件的样式入口文件的名字由__index__.css改为piano_keyboard.css，以解决将网站部署到github pages上时，无法加载名为__index__.css文件的问题

--- src/component/piano-keyboard/web_component/piano-keyboard-style.ts
+++ src/component/piano-keyboard/web_component/piano-keyboard-style.ts
@@ -1,4 +1,4 @@
-const styleUrl=(new URL('./__index__.css', import.meta.url)).href;
+const styleUrl=(new URL('./piano_keyboard.css', import.meta.url)).href;
 
 export const pianoKeyboardStyle=/*html*/`
 <link rel="stylesheet" href="${styleUrl}">
--- src/server/MusicService.ts
+++ src/server/MusicService.ts
@@ -5,42 +5,67 @@
 
 
 
+
 export class MusicService {
     _sampler:Tone.Sampler
-
-
+    
+    
     constructor() {
-
+        const _samplerSounds={
+            "C2":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/C2v5.ogg', import.meta.url)).href,
+            "C3":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/C3v5.ogg', import.meta.url)).href,
+            "C4":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/C4v5.ogg', import.meta.url)).href,
+            "C5":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/C5v5.ogg', import.meta.url)).href,
+            "C6":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/C6v5.ogg', import.meta.url)).href,
+            
+            "Ds2":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Ds2v5.ogg', import.meta.url)).href,
+            "Ds3":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Ds3v5.ogg', import.meta.url)).href,
+            "Ds4":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Ds4v5.ogg', import.meta.url)).href,
+            "Ds5":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Ds5v5.ogg', import.meta.url)).href,
+            "Ds6":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Ds6v5.ogg', import.meta.url)).href,
+            
+            "Fs2":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Fs2v5.ogg', import.meta.url)).href,
+            "Fs3":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Fs3v5.ogg', import.meta.url)).href,
+            "Fs4":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Fs4v5.ogg', import.meta.url)).href,
+            "Fs5":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Fs5v5.ogg', import.meta.url)).href,
+            "Fs6":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/Fs6v5.ogg', import.meta.url)).href,
+            
+            "A2":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/A2v5.ogg', import.meta.url)).href,
+            "A3":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/A3v5.ogg', import.meta.url)).href,
+            "A4":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/A4v5.ogg', import.meta.url)).href,
+            "A5":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/A5v5.ogg', import.meta.url)).href,
+            "A6":(new URL('../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/A6v5.ogg', import.meta.url)).href
+        }
+        
         // console.log(Tone);
 
         this._sampler = new Tone.Sampler({
             urls: {
-                "C2": "C2v5.ogg",
-                "C3": "C3v5.ogg",
-                "C4": "C4v5.ogg",
-                "C5": "C5v5.ogg",
-                "C6": "C6v5.ogg",
+                "C2": _samplerSounds["C2"],
+                "C3": _samplerSounds["C3"],
+                "C4": _samplerSounds["C4"],
+                "C5": _samplerSounds["C5"],
+                "C6": _samplerSounds["C6"],
 
-                "D#2": "Ds2v5.ogg",
-                "D#3": "Ds3v5.ogg",
-                "D#4": "Ds4v5.ogg",
-                "D#5": "Ds5v5.ogg",
-                "D#6": "Ds6v5.ogg",
+                "D#2":_samplerSounds["Ds2"],
+                "D#3":_samplerSounds["Ds3"],
+                "D#4":_samplerSounds["Ds4"],
+                "D#5":_samplerSounds["Ds5"],
+                "D#6":_samplerSounds["Ds6"],
 
-                "F#2": "Fs2v5.ogg",
-                "F#3": "Fs3v5.ogg",
-                "F#4": "Fs4v5.ogg",
-                "F#5": "Fs5v5.ogg",
-                "F#6": "Fs6v5.ogg",
+                "F#2":_samplerSounds["Fs2"],
+                "F#3":_samplerSounds["Fs3"],
+                "F#4":_samplerSounds["Fs4"],
+                "F#5":_samplerSounds["Fs5"],
+                "F#6":_samplerSounds["Fs6"],
 
-                "A2": "A2v5.ogg",
-                "A3": "A3v5.ogg",
-                "A4": "A4v5.ogg",
-                "A5": "A5v5.ogg",
-                "A6": "A6v5.ogg",
+                "A2": _samplerSounds["A2"],
+                "A3": _samplerSounds["A3"],
+                "A4": _samplerSounds["A4"],
+                "A5": _samplerSounds["A5"],
+                "A6": _samplerSounds["A6"],
             },
             release: 1,
-            baseUrl: "../../assets/audio/piano_samples/SalamanderGrandPianoV3_OggVorbis/",
         }).toDestination();
 
     }
